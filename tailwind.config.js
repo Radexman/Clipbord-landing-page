030aa57b31332220c98933f90d1e652566bdb760
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-05-21 10:43:05 +0200

    Theme Setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
 	content: ['dist/*html'],
 	theme: {
-		extend: {},
+		screen: {
+			sm: '480px',
+			md: '768px',
+			lg: '976px',
+			xl: '1440px',
+		},
+		extend: {
+			colors: {
+				'strong-cyan': 'hsl(171, 66%, 44%)',
+				'light-blue': 'hsl(223, 100%, 69%)',
+				'dark-gray-blue': 'hsl(219, 10%, 33%)',
+				'grayish-blue': 'hsl(201, 11%, 66%)',
+			},
+			fontFamily: {
+				sans: ['Bai Jamjuree', 'sans-serif'],
+			},
+		},
 	},
 	plugins: [],
 };
